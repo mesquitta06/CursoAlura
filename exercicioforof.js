911b36e9d86b2ba33a0ef4f456fd37ac759592e8
Otavio Ferreira De Mesquita <otaviom.prestserv@vibraenergia.com.br>
2023-10-10 17:02:32 -0300

    Finalizando array, entrando em callback

---
+++ exercicioforof.js
@@ -0,0 +1,7 @@
+const numeros = []
+
+for (let i =0 ; i <= 1000; i +=100){
+    numeros.push(i);
+}
+
+console.log(numeros)
--- mediaforof.js
+++ mediaforof.js
@@ -1,1 +1,12 @@
-const notas = [10, 6.5 , 8 , 7.5]
+const notas = [10, 6.5 , 8 , 7.5, 7, 12.3, 25, 69]
+
+let somaDasNotas = 0;
+
+for(let elemento of notas){ //para cada elemento de notas, execute o codigo dentro das chaves
+  somaDasNotas += elemento
+
+}
+
+media = somaDasNotas/ notas.length
+
+console.log(media)
