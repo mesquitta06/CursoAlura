7e3384cda1b34417085c8405f5b59c3be47f5419
Otavio Ferreira De Mesquita <otaviom.prestserv@vibraenergia.com.br>
2023-10-10 16:14:05 -0300

    atualizando estudos

---
+++ for.js
@@ -0,0 +1,9 @@
+const numbers = [100, 200, 300 , 400 , 500 ,600]
+
+//Primeira expressao é executada apenas uma vez
+//condicao de execução 
+//É esxecutda ao final do bloco
+
+for (let indice = 0 ; indice < numeros.length; indice ++){
+    console.log(numbers[indice]);
+}
--- procurandoAluno.js
+++ procurandoAluno.js
@@ -1,18 +1,29 @@
-const alunos = ["João", "Juliana", "Ana", "Caio"];
+const alunos = ["João", "Juliana", "Ana", "Caio", "André"];
 const medias = [10, 8, 7.5, 9];
 
 const listaAlunoEmedias = [alunos, medias]
 
 
 
-const aluno = "Caio"
+const aluno = "André"
 
 function exibeNomeENota(aluno){
     if(listaAlunoEmedias[0].includes(aluno)){
+
+        //const alunos = listaAlunoEmedias[0]
+        //const medias = listaAlunoEmedias[1];
+
+        const [alunos, medias ] = listaAlunoEmedias
+
+        const indice = alunos.indexOf(aluno);
+        const mediaAluno = medias[indice];
         
-        const indice = listaAlunoEmedias [0].indexOf(aluno);//index of mostar o indice de um elemento no array
-        const mediaDoAluno = listaAlunoEmedias[1][indice];
-        console.log(`O aluno(a) ${aluno} está cadastrado , e sua nota é ${mediaDoAluno}`);
+        
+        //const indice = listaAlunoEmedias [0].indexOf(aluno);//index of mostar o indice de um elemento no array
+
+        //const mediaDoAluno = listaAlunoEmedias[1][indiceAluno];
+
+        console.log(`O aluno(a) ${aluno} está cadastrado , e sua nota é ${mediaAluno}`);
 
 
     }else{
